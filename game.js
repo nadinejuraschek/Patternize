3183ad9e10149c7cdeb02d10129c9d55324ea293
Nadine Ju <nadine.jura@online.de>
2019-12-07 14:29:41 -0800

    playSound function fixed, level increase coded

--- game.js
+++ game.js
@@ -6,15 +6,19 @@ var userClickedPattern = [];
 
 var randomNum = 0;
 var nextColor = "";
+var level = 0;
+var started = false;
 
 function nextSequence() {
+    level++;
+    $('#level-title').html("Level " + level);
     randomNum = Math.floor(Math.random() * 4);
     console.log("randomNumber: " + randomNum);
-    return randomNum;
+    randomChosenColor();
+    started = !started;
 };
 
 function randomChosenColor() {
-    nextSequence();
     var nextColor = btnColors[randomNum];
     console.log("Next Color: " + nextColor);
     gamePattern.push(nextColor);
@@ -27,14 +31,22 @@ function randomChosenColor() {
 };
 
 function playSound(name) {
-    // var audio = new Audio("sounds/" + nextColor + ".mp3");
-    // audio.play();
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 };
 
-randomChosenColor();
+$(document).keyup(function (e){
+    if (!started) {
+        nextSequence();
+        $('#level-title').html("Level " + level);
+        started = true;
+    };
+});
 
 $('.btn').click(function() {
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     console.log(userClickedPattern);
+    playSound(userChosenColor);
+    $(this).fadeIn(100).fadeOut(100).fadeIn(100);
 });
