ac1bc908f4df884e77f470c8c2f349eca9c2ada7
Nadine Ju <nadine.jura@online.de>
2019-12-07 10:57:00 -0800

    flash effect coded

--- game.js
+++ game.js
@@ -1,6 +1,26 @@
 console.log("Connected");
 
+var btnColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+var randomNum = 0;
+var nextColor = "";
+
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4);
-    console.log("randomNumber: " + randomNumber);
+    randomNum = Math.floor(Math.random() * 4);
+    console.log("randomNumber: " + randomNum);
+    return randomNum;
 };
+
+function randomChosenColor() {
+    nextSequence();
+    var nextColor = btnColors[randomNum];
+    console.log("Next Color: " + nextColor);
+    gamePattern.push(nextColor);
+    console.log(gamePattern);
+    var selectedSquare = $("#" + nextColor);
+    console.log(selectedSquare);
+    selectedSquare.fadeIn(100).fadeOut(100).fadeIn(100);
+};
+
+randomChosenColor();
